c30c3e9cbf08ff29c52b2761cf9be19810010e64
wangjia <jawa0919@163.com>
2021-07-20 09:45:02 +0800

    init

---
+++ lib/wizjs_flutter_lib.dart
@@ -0,0 +1,7 @@
+library wizjs_flutter_lib;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ wizjs_flutter_lib_example/android/app/src/main/kotlin/com/example/wizjs_flutter_lib_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wizjs_flutter_lib_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
